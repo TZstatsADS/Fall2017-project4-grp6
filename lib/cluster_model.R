cd3c5b1492a99e74aa812b2759645acabd7f299c
duanshiqi <duanshiqi@users.noreply.github.com>
2017-11-19 15:47:01 -0500

    process dataset2: fix score range to 0-5

---
+++ lib/cluster_model.R
@@ -0,0 +1,3 @@
+setwd("/Users/duanshiqi/Documents/GitHub/fall2017-project4-fall2017-proj4-grp6/data/")
+train <- read.csv("../output/dataset2_train.csv",header=T)
+test <- read.csv("../output/dataset2_test.csv",header=T)
