8290def7d0b8d9094b13351c9fb3977ce435be65
Saaya Yasuda <saaya.yasuda@gmail.com>
2017-11-22 19:57:27 -0500

    committing the changes to similarity_weights.r

--- lib/similarity_weights.r
+++ lib/similarity_weights.r
@@ -17,7 +17,6 @@ if(length(packages.needed)>0){
 }
 library(lsa)
 
-
 #######################################
 # Load data
 #######################################
@@ -26,68 +25,64 @@ test1 <- read.csv("./output/dataset1_test.csv",header=T)
 train2 <- read.csv("./output/dataset2_train.csv",header=T)
 test2 <- read.csv("./output/dataset2_test.csv",header=T)
 
-rownames(train1) <- train1[,1]
-train1 <- train1[,-1]
-rownames(train2) <- train2[,1]
-train2 <- train2[,-1]
+rownames(train1) = train1[,1]
+train1 = train1[,-1]
+
+rownames(train2) = train2[,1]
+train2 = train2[,-1]
 
 #######################################
 # Spearman
 #######################################
 
-spearman_weight <- function(user_vec_a, user_vec_u){
-  rank_a = rank(user_vec_a, ties.method = 'average')
-  rank_u = rank(user_vec_u, ties.method = 'average')
-  ranks = cbind(rank_a, rank_u)
-  w <- cov(ranks) / (sd(rank_a) * sd(rank_u))
-  return(w)
+spearman_weight <- function(matrix){
+  file=deparse(substitute(matrix))
+  file_name= paste0("./output/spearman_",file,".csv")
+  
+  matrix[is.na(matrix)] = 0
+  matrix = t(matrix)
+  w = cor(matrix,use="everything",method="spearman")
+  
+  write.csv(w,file=file_name)
 }
 
-w1 = spearman_weight(train1[1,], train1[2,])
-w2 = spearman_weight(train2[1,], train2[2,])
-w1
-w2
-w1[1,2]
-w2[1,2]
-#cor.test(x=unlist(train1[1,]), y=unlist(train1[2,]), method = 'spearman')
+w1 = spearman_weight(train1)
+w2 = spearman_weight(train2)
 
 #######################################
 # Vector Similarity
 #######################################
 
-vector_similarity <- function(user_vec_a, user_vec_u){
-  user_vec_a = unlist(user_vec_a,use.names=FALSE)
-  user_vec_u = unlist(user_vec_u,use.names=FALSE)
-  #w = crossprod(user_vec_a, user_vec_u)/sqrt(crossprod(user_vec_a) * crossprod(user_vec_u))
+vector_similarity <- function(matrix){
   library(lsa)
-  w = cosine(user_vec_a, user_vec_u)
-  return(w)
+  
+  file=deparse(substitute(matrix))
+  file_name= paste0("./output/vectorsimilarity_",file,".csv")
+  
+  matrix[is.na(matrix)] = 0
+  matrix = t(matrix)
+  w = cosine(matrix)
+  write.csv(w,file=file_name)
 }
 
-w1 = vector_similarity(train1[1,], train1[2,])
-w2 = vector_similarity(train2[1,], train2[2,])
-w1
-w2
-#0 and NA
-w1[1,2]
-w2[1,2]
-
-#debug
-user_vec_a = train1[1,]
-user_vec_u = train1[2,]
-typeof(user_vec_a)
-user_vec_a = unlist(user_vec_a,use.names=FALSE)
-user_vec_u = unlist(user_vec_u,use.names=FALSE)
-cosine(user_vec_a, user_vec_u)
+w1 = vector_similarity(train1)
+w2 = vector_similarity(train2)
 
 
 #######################################
-# Mean Squared Difference
+# Mean Squared Difference - Work in Progress
 #######################################
 
+# old ver
 mean_sq_diff <- function(user_vec_a, user_vec_u){
   msd = mean((user_vec_a - user_vec_u)^2)
   return(msd)
+}
+
+
+mean_sq_diff <- function(matrix){
+  #msd = mean((user_vec_a - user_vec_u)^2)
+  #return(msd)
 }
 
 w1 = mean_sq_diff(train1[1,], train1[2,])
