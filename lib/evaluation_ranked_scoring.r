d1d839632ac08d5fa89448b67bad65ada4f3d985
Saaya Yasuda <saaya.yasuda@gmail.com>
2017-11-29 18:44:38 -0500

    update

--- lib/evaluation_ranked_scoring.r
+++ lib/evaluation_ranked_scoring.r
@@ -16,12 +16,15 @@
 #n <- 3
 #matrix = round(matrix(runif(m * n), m, n))
 
+
+
 randked_scoring <- function(pred_matrix, observed_matrix){
   nrow = nrow(pred_matrix)
   ncol = ncol(pred_matrix)
   a_minus_one = nrow/2 - 1
   
   # rank/sort pred_matrix by vote value. Assuming d=0 here.
+  pred_matrix[pred_matrix<0] = 0
   numerator_matrix = t(apply(pred_matrix, 1, sort,decreasing=T))
   
   # denominator of r_a
@@ -46,10 +49,36 @@ randked_scoring <- function(pred_matrix, observed_matrix){
 }
 
 ### for testing:
-#m <- 4
-#n <- 3
-#observed_matrix = round(matrix(runif(m * n), m, n))
-#pred_matrix = matrix(runif(m * n), m, n)
+m <- 4
+n <- 3
+observed_matrix = round(matrix(runif(m * n), m, n))
+pred_matrix = matrix(runif(m * n), m, n)
+
+nrow = nrow(pred_matrix)
+ncol = ncol(pred_matrix)
+a_minus_one = nrow/2 - 1
+
+# rank/sort pred_matrix by vote value. Assuming d=0 here.
+pred_matrix[pred_matrix<0] = 0
+numerator_matrix = t(apply(pred_matrix, 1, sort,decreasing=T))
+
+# denominator of r_a
+denom_vector = rep(NA, ncol)
+for (j in 1:ncol){
+  denom_vector[j] <- 2^((j-1)/a_minus_one)
+}
+
+# get a vector of r_a
+utility_matrix = numerator_matrix %*% diag(1 / denom_vector)
+r_a_vector = rowSums(utility_matrix)
+
+# get a r_a_max value
+max_numerator_matrix = t(apply(observed_matrix, 1, sort,decreasing=T))
+max_utility_matrix = max_numerator_matrix %*% diag(1 / denom_vector)
+max_r_a_vector = rowSums(max_utility_matrix)
+
+# Get the r_a / r_a_max score
+r = 100 * sum(r_a_vector)/sum(max_r_a_vector)
 
 #randked_scoring(pred_matrix,observed_matrix)
 
